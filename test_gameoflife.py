fc05cf2153e08f6558c46923c68759328cf0ee55
athul <kpathul@outlook.com>
2019-02-15 13:40:04 +0530

    Red: Adds test for existential_check()

--- test_gameoflife.py
+++ test_gameoflife.py
@@ -1,11 +1,23 @@
 import gameoflife
 
-def test_neighbour_finder():
+def test_existential_check():
+    row, col = 2,2 
+    # when m is out of index
+    n, m = 1,3
+    assert gameoflife.existential_check(n,m,row,col) == False
+    # when n is out of indec
+    n, m = -1,1
+    assert gameoflife.existential_check(n,m,row,col) == False
+    # when n and m are in index
+    n, m = 1,2
+    assert gameoflife.existential_check(n,m,row,col) == True
 
+
+def test_neighbour_finder():
     neighbour_hood = [ [False, True],
                        [True, False] ]
-
     result =  [ [2,1],
                 [1,2] ]
 
     assert gameoflife.neighbour_finder(neighbour_hood) == result
+
