25c6ec88bdb9e888e68653427ea8ae3bdc3fc47c
athul <kpathul@outlook.com>
2019-02-15 14:00:15 +0530

    Red: Creates existential_check() and neighbour_finder()

---
+++ gameoflife.py
@@ -0,0 +1,58 @@
+# Conway's Game of Life
+
+def existential_check(a,b,row,col): 
+    "Checks whether the calling neighbour(a,b) exists or not"
+    if a in range(row+1) and b in range(col+1):    
+        return True
+    else:
+        return False
+
+def neighbour_finder(grid):
+    "neighbour_finder() finds number of alive cells for each cells (dead or alive)"
+    rows = len(grid)
+    cols = len(grid[0])
+
+    result = [ [ [0] ]*cols ]*rows
+
+    for i in range(rows):
+        for j in range(cols):
+            count = 0
+            #Counting:
+            if existential_check(i-1,j-1,rows,cols):
+                if grid[i-1][j-1]:
+                    count += 1
+            if existential_check(i-1,j,rows,cols):
+                if grid[i-1][j]:
+                    count += 1
+            if existential_check(i-1,j+1,rows,cols):
+                if grid[i-1][j+1]:
+                    count += 1
+
+            if existential_check(i,j-1,rows,cols):
+                if grid[i][j-1]:    
+                    count += 1
+            if existential_check(i,j+1,rows,cols):
+                if grid[i][j+1]:    
+                    count += 1
+
+            if existential_check(i+1,j-1,rows,cols):
+                if grid[i+1][j-1]:    
+                    count += 1
+            if existential_check(i+1,j,rows,cols):
+                if grid[i+1][j]:    
+                    count += 1
+            if existential_check(i+1,j+1,rows,cols):
+                if grid[i+1][j+1]:    
+                    count += 1
+
+            result[i][j] = count
+
+    return result
+
+
+
+                    
+                
+                
+
+                
