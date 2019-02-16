0117246f81ab66963a6527ba695af8cff8f26be8
athul <kpathul@outlook.com>
2019-02-16 14:46:15 +0530

    Red: Adds test for string_to_matrix

--- test_RLEsupport.py
+++ test_RLEsupport.py
@@ -11,3 +11,9 @@ def test_RLE_to_string():
     val ="2b3o3$o4b!"
     result = "False False True True True ;False ;False ;True False False False False"
     assert RLEsupport.RLE_to_string(val) == result
+
+
+def test_string_to_matrix():
+    value =  "True False; True False"
+    rslt  = [[True, False],[True, False]]
+    assert RLEsupport.string_to_matrix(value) == rslt
