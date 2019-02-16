bd6571174dc13996e39cd7ac40e81cd32f0890fd
athul <kpathul@outlook.com>
2019-02-16 19:01:16 +0530

    Adds Pulsar and Gosper-glider-gun configs

--- other_configurations.py
+++ other_configurations.py
@@ -3,9 +3,17 @@ from time import sleep
 import RLEsupport
 
 
-# 1. Beacon: works
-rle ="2o2b$o3b$3bo$2b2o!"
-x,y = 4,4
+# 1. Beacon
+# rle ="2o2b$o3b$3bo$2b2o!"
+# x,y = 4,4
+
+# 2. Pulsar
+# rle ="2b3o3b3o2b2$o4bobo4bo$o4bobo4bo$o4bobo4bo$2b3o3b3o2b2$2b3o3b3o2b$o4bobo4bo$o4bobo4bo$o4bobo4bo2$2b3o3b3o!"
+# x,y = 13,13
+
+# 3. Gosper glider gun
+rle = "24bo$22bobo$12b2o6b2o12b2o$11bo3bo4b2o12b2o$2o8bo5bo3b2o$2o8bo3bob2o4bobo$10bo5bo7bo$11bo3bo$12b2o!"
+x, y = 36, 9
 
 
 
