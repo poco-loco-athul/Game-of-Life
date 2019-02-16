e99a5716ec620fd6fd50652548b972d010cbd1ae
athul <kpathul@outlook.com>
2019-02-16 14:42:04 +0530

    Green: Extends the test for RLE_to_string()

--- test_RLEsupport.py
+++ test_RLEsupport.py
@@ -2,6 +2,12 @@ import RLEsupport
 
 
 def test_RLE_to_string():
+    # Test for replacing of b, o and $
     val = "2o$2b!"
     result = "True True ;False False"
+    assert RLEsupport.RLE_to_string(val) == result
+    
+    # Test for empty lines
+    val ="2b3o3$o4b!"
+    result = "False False True True True ;False ;False ;True False False False False"
     assert RLEsupport.RLE_to_string(val) == result
