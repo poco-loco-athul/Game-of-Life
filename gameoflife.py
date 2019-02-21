f5f2e55937e5239f14dc1df9736b7efdc1d6262e
athul <kpathul@outlook.com>
2019-02-22 00:10:00 +0530

    Refactors: gameoflife
    Makes changes based on comments from last commit
    1. Renames functions and variables
    2. Makes some more functions to increase readablity
    3. Increases codes-rating using pylint

--- gameoflife.py
+++ gameoflife.py
@@ -1,76 +1,80 @@
 # Conway's Game of Life
+"gameoflife contains functions to simulate Conway's Game of Life"
 
-def existential_check(a,b,row,col):  # A better name would be "within_bounds"
+def within_bounds(m_row, n_col, rows, cols):
     """Checks whether the calling cell(a,b) exists or not. This helps
-    neighbour_finder() stay inside the given matrix.""" # Use multiline docstrongs
-    return all([a in range(row), #better  style
-                b in range(col)])
+    count_of_alive_neighbours() stay inside the given matrix."""
+    return all([m_row in range(rows),
+                n_col in range(cols)])
 
+def make_empty_matrix(m_row, n_col):
+    "Makes empty matrix with 'm' rows and 'n' columns"
+    return [[None for i in range(n_col)] for j in range(m_row)]
 
-def neighbour_finder(grid):
-    "neighbour_finder() finds number of neighbouring alive cells for each cell"
-    rows = len(grid)
-    cols = len(grid[0])
-    result = [[None for i in range(cols)] for j in range(rows)]  # Put this in a function make_empty_grid(cols, rows)
-    
-    for i in range(rows):
-        for j in range(cols):
+def all_neighbours(m_row, n_col):
+    "Returns all 8 neighbours, even out bounds"
+    list_a = [m_row + x for x in [-1, 0, 1]]
+    list_b = [n_col + y for y in [-1, 0, 1]]
+    neighbours = [(m, n) for m in list_a for n in list_b]
+    neighbours.remove((m_row, n_col))
+    return neighbours
+
+def count_of_alive_neighbours(grid):
+    "Returns number of neighbouring alive cells for each cell"
+    rows, cols = len(grid), len(grid[0])
+    count_matrix = make_empty_matrix(rows, cols)
+    for row in range(rows):
+        for col in range(cols):
             count = 0
             # A cell can have 8 possible neighbours
             # if cell exists, if cell is alive, count increments
-            cells = [(i-1,j-1), (i-1, j), (i-1,j+1),
-                     (i,  j-1),          (i,j+1),
-                     (i+1,j-1), (i+1,j), (i+1,j+1)]
-            for k in cells:
-                if existential_check(k[0],k[1],rows,cols): 
-                    if grid[ k[0] ][ k[1] ]:
+            cells = all_neighbours(row, col)
+            for cel in cells:
+                if within_bounds(cel[0], cel[1], rows, cols):
+                    if grid[cel[0]][cel[1]]:
                         count += 1
-            result[i][j] = count
-    return result
+            count_matrix[row][col] = count
+    return count_matrix
 
-
-def apply_rules(grd,neighbour):
+def apply_rules(grid, neighbour):
     "Applies Conway's rules and produces next generation"
-    rows = len(grd)
-    cols = len(grd[0])    
-    for i in range(rows):
-        for j in range(cols):
+    rows, cols = len(grid), len(grid[0])
+    for row in range(rows):
+        for col in range(cols):
             # Rules 1,2 and 3
-            if grd[i][j]:
-                if neighbour[i][j] not in [2,3]: # BEtter than using range
-                    grd[i][j] = False
-            # Rule 4        
-            if not grd[i][j]:
-                if neighbour[i][j] == 3:
-                    grd[i][j] = True
-    return grd
+            if grid[row][col]:
+                if neighbour[row][col] not in [2, 3]:
+                    grid[row][col] = False
+            # Rule 4
+            if not grid[row][col]:
+                if neighbour[row][col] == 3:
+                    grid[row][col] = True
+    return grid
 
-                    
+
 def display(grd):
     "Displays given grid(matrix) as a string"
-    rows = len(grd)
-    cols = len(grd[0])    
-    formatter = ""
+    rows, cols = len(grd), len(grd[0])
+    dsply_string = ""
     for i in range(rows):
-        formatter += "\t\n"
+        dsply_string += "\t\n"
         for j in range(cols):
             if grd[i][j]:
-                formatter += "O "
+                dsply_string += "O "
             else:
-                formatter += ". "
-    return formatter
+                dsply_string += ". "
+    return dsply_string
 
 
-def display_curses(grd, r=5):
+def display_curses(grd, disp=5):
     "Changes given matrix as a dict that contains position and character"
-    # r is the shift from 0,0 of screen
-    rows = len(grd)
-    cols = len(grd[0])    
-    output = { }
+    # disp is the shift from 0,0 of screen
+    rows, cols = len(grd), len(grd[0])
+    dsply_dict = {}
     for i in range(rows):
         for j in range(cols):
             if grd[i][j]:
-                output[(i+r,j+r)] = "O"
+                dsply_dict[(i+disp, j+disp)] = "O"
             else:
-                output[(i+r,j+r)] = "."
-    return output
+                dsply_dict[(i+disp, j+disp)] = "."
+    return dsply_dict
