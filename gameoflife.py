1e133ff6506e7840e919c5b1c89745be491e463c
Noufal Ibrahim <noufal@nibrahim.net.in>
2019-02-21 17:19:50 +0530

    Comments

--- gameoflife.py
+++ gameoflife.py
@@ -1,25 +1,26 @@
 # Conway's Game of Life
 
-def existential_check(a,b,row,col): 
-    "Checks whether the calling cell(a,b) exists or not. This helps neighbour_finder() stay inside the given matrix. "
-    if a in range(row) and b in range(col):    
-        return True
-    else:
-        return False
-    
+def existential_check(a,b,row,col):  # A better name would be "within_bounds"
+    """Checks whether the calling cell(a,b) exists or not. This helps
+    neighbour_finder() stay inside the given matrix.""" # Use multiline docstrongs
+    return all([a in range(row), #better  style
+                b in range(col)])
+
 
 def neighbour_finder(grid):
     "neighbour_finder() finds number of neighbouring alive cells for each cell"
     rows = len(grid)
     cols = len(grid[0])
-    result = [[None for i in range(cols)] for j in range(rows)]
+    result = [[None for i in range(cols)] for j in range(rows)]  # Put this in a function make_empty_grid(cols, rows)
     
     for i in range(rows):
         for j in range(cols):
             count = 0
             # A cell can have 8 possible neighbours
             # if cell exists, if cell is alive, count increments
-            cells = [(i-1,j-1), (i-1, j), (i-1,j+1), (i,j-1), (i,j+1), (i+1,j-1), (i+1,j), (i+1,j+1)]
+            cells = [(i-1,j-1), (i-1, j), (i-1,j+1),
+                     (i,  j-1),          (i,j+1),
+                     (i+1,j-1), (i+1,j), (i+1,j+1)]
             for k in cells:
                 if existential_check(k[0],k[1],rows,cols): 
                     if grid[ k[0] ][ k[1] ]:
@@ -36,7 +37,7 @@ def apply_rules(grd,neighbour):
         for j in range(cols):
             # Rules 1,2 and 3
             if grd[i][j]:
-                if neighbour[i][j] not in range(2,4):
+                if neighbour[i][j] not in [2,3]: # BEtter than using range
                     grd[i][j] = False
             # Rule 4        
             if not grd[i][j]:
